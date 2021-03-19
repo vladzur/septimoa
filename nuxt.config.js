7a71a6b61e0865c69ea9be1e88c79dbd5e4a3940
Vladzur <vladzur@gmail.com>
2021-03-19 17:44:33 -0300

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - septimoa',
-    title: 'septimoa',
+    titleTemplate: '%s - Séptimo A',
+    title: 'Séptimo A',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
